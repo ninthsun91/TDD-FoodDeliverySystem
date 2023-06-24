35a917acdd28b405316f28c49d496c776b0202fe
jkim9409 <92039854+jkim9409@users.noreply.github.com>
2023-06-24 23:31:30 +0900

    :fire Delete order module, controller, service"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
-    UsersModule,
-    OrdersModule
+    UsersModule
   ],
   controllers: [],
   providers: [],
