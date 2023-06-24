12d1361392d126a818874d9703ac97abeb2b86e0
jkim9409 <92039854+jkim9409@users.noreply.github.com>
2023-06-25 00:04:10 +0900

    :test_tube: 오더 생성 실패 테스트 케이스 작성

--- src/orders/orders.service.spec.ts
+++ src/orders/orders.service.spec.ts
@@ -15,4 +15,68 @@ describe('OrdersService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('Order Creation', () => {
+    describe('Order Creates normally', () => {
+      it('should return id value of the created order.', () => {
+        const order = undefined;
+        const result = undefined;
+        expect(result).toBe(true);
+      });
+
+      it('should call save() function in the order-repository', () => {
+        const order = undefined;
+        const result = undefined;
+        expect(result).toBe(true);
+      });
+    });
+
+    describe('Check general validation requirement', () => {
+      it('should return error if business type user tries to create order', () => {
+        const order = undefined;
+        const result = undefined;
+        expect(result).toBe(true);
+      });
+
+      it('should return error if it tries to create order to a restaurant that does not exist', () => {
+        const order = undefined;
+        const result = undefined;
+        expect(result).toBe(true);
+      });
+
+      it('should return error if it tries to create order of a menu item that does not exist', () => {
+        const order = undefined;
+        const result = undefined;
+        expect(result).toBe(true);
+      });
+
+    });
+
+    describe('Check businsess specific requirement', () => {
+      it('should return error if an order consists of more than 10 menu items', () => {
+        const order = undefined;
+        const result = undefined;
+        expect(result).toBe(true);
+      });
+
+      it('should return error if an order consists of 0 menu items', () => {
+        const order = undefined;
+        const result = undefined;
+        expect(result).toBe(true);
+      });
+
+      it('should return error if the user already have another order that is still processing', () => {
+        const order = undefined;
+        const result = undefined;
+        expect(result).toBe(true);
+      });
+
+      it('should return error if a menu item does not have enough stocks', () => {
+        const order = undefined;
+        const result = undefined;
+        expect(result).toBe(true);
+      });
+    });
+
+  });
 });
